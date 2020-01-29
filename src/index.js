97cbea5695931bcf3023a97842a16b7dcdddaf3e
Raymond Sotto <raymondjosephsotto@gmail.com>
2020-01-28 23:19:28 -0500

    Added Redux to the BurgerBuilder App

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {BrowserRouter} from 'react-router-dom';
-
+import reducer from './store/reducer';
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
@@ -12,10 +15,14 @@ import {BrowserRouter} from 'react-router-dom';
 serviceWorker.unregister();
 
 
+const store = createStore(reducer)
+
 const app = (
+   <Provider store={store}>
     <BrowserRouter>
         <App/>
     </BrowserRouter>
+   </Provider>
 );
 
 ReactDOM.render(app, document.getElementById('root'));
